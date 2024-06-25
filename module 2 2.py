850495c3a54f93817d0cc44997f14492a6f68505
Marina <smv1106@gmail.com>
2024-06-25 17:44:15 +0300

    Add files via upload

---
+++ module 2 2.py
@@ -0,0 +1,28 @@
+fist = (123)
+print(fist)
+second = (456)
+print(second)
+third = (789)
+print(third)
+if fist == second and second == third:
+    print(3)
+elif fist == second or fist == third or second == third:
+    print(2)
+elif fist != second and second != third:
+    print(0)
+#
+fist = (42)
+print(fist)
+second = (69)
+print(second)
+third = (42)
+print(third)
+if fist == second and second == third:
+    print(3)
+elif fist == second or fist == third or second == third:
+    print(2)
+else:
+    print(0)
+
+
+
