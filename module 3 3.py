c0e6eb4819c910937ee9174595cd2826d4dbd271
Marina <smv1106@gmail.com>
2024-07-16 18:13:30 +0300

    Update module 3 3.py

--- module 3 3.py
+++ module 3 3.py
@@ -1,15 +1,20 @@
 def print_params(a=1, b='строка', c=True):
     print(a, b, c)
-print_params(b = 5)
+    print(a, c)
+    print(b)
+    print()
+
+print_params()
+print_params(b = 25)
 print_params(c = [1, 2, 3])
 
-value_list = [2, 'подстрока', False]
-value_dict = {'a':'start', 'b':'stop', 'c':'finish'}
-value_list_2 = [54.32, 'Строка']
+value_list = [2, 3.4, 'odin']
+value_dict = {'a':'start', 'b':3, 'c': 1.5}
+value_list_2 = [1, 2.3]
 
 print_params(*value_list)
 print_params(**value_dict)
-print_params(*value_list_2, 42)
+print_params(*value_list_2, "lol")
 
 
 
