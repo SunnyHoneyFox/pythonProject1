ac29d35300e7a28ec8d135912533767a42429881
Marina <smv1106@gmail.com>
2024-07-30 16:20:42 +0300

    Add files via upload

---
+++ module 4 2.py
@@ -0,0 +1,17 @@
+def test_function():
+
+    def inner_fuction():
+        print('Я в области видимости функции test_function')
+    inner_fuction()
+test_function()
+
+
+def test_function():
+
+    def inner_fuction():
+        print('Я в области видимости функции test_function')
+    inner_fuction()
+
+
+
+
