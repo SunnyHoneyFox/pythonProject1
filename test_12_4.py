e9583ffba8aaf733f9bc795a4acb6676935053cb
Marina <smv1106@gmail.com>
2024-11-24 03:08:50 +0300

    Add files via upload

---
+++ test_12_4.py
@@ -0,0 +1,98 @@
+import unittest
+import logging
+
+logging.basicConfig(
+    filename='runner_tests.log',
+    filemode='w',
+    encoding='utf-8',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
+        self.distance = 0
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+
+class RunnerTest(unittest.TestCase):
+    all_results = []
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = []
+
+    def setUp(self):
+        self.runner1 = Runner('Усейн', speed=10)
+        self.runner2 = Runner('Андрей', speed=9)
+        self.runner3 = Runner('Ник', speed=3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in cls.all_results:
+            print(result)
+
+    def test_walk(self):
+        try:
+
+            invalid_runner = Runner('Ник', speed=-5)
+            logging.info('"test_walk" выполнен успешно')
+        except ValueError as e:
+            logging.warning('Неверная скорость для Runner: %s', e)
+
+    def test_run(self):
+        try:
+
+            invalid_runner = Runner(123, speed=10)
+            logging.info('"test_run" выполнен успешно')
+        except TypeError as e:
+            logging.exception('Неверный тип данных для объекта Runner')
+
+
+if __name__ == '__main__':
+    unittest.main()
